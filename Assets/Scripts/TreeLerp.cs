47a82985b1e69f591e7e8a87ed13b39a2d9f3f82
TheShadowRaptor <90637945+TheShadowRaptor@users.noreply.github.com>
2021-11-19 15:21:41 -0400

    Initial commit

---
+++ Assets/Scripts/TreeLerp.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TreeLerp : MonoBehaviour
+{
+    //=====================================Initalize========================================
+    //initalize maximum and minimum values of Lerp
+
+    //for X transform value
+    public float minimumX = 0;
+    public float maximumX = 0;
+
+    //for Y transform value
+    public float minimumY = 0;
+    public float maximumY = 0;
+
+    //initalize time it takes to complete Lerp
+    public float t = 0;
+
+    //initalize positon of Lerped Object
+    public float posX;
+    public float posY;
+
+    //initalize Lerp bool
+    public bool lurp = true;
+
+    //======================================+=========================================
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //==================================Lerping======================================
+        if (lurp == true)
+        {
+            transform.position = new Vector2(Mathf.Lerp(minimum, maximum, t), Mathf.Lerp(min)
+        }
+
+        //======================================+=========================================
+
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# GameJam_RailShooter
+ 
