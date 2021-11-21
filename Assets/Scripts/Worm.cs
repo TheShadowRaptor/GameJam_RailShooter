69f56504f7feda13bfe245681fe73095115a4a5e
TheShadowRaptor <90637945+TheShadowRaptor@users.noreply.github.com>
2021-11-21 00:25:22 -0400

    Bringing Home through git

---
+++ Assets/Scripts/Worm.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Worm : MonoBehaviour
+{
+
+    private float maxScaleX;
+    private float macScaleY;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //change Input.GetKeyDown to when the enemys gets behind the screen
+        if (this.transform.localScale.x >= maxScaleX)
+        {
+            hearts = hearts - damageTaken;
+            Debug.Log("YOU TOOK DAMAGE, " + hearts + " Hearts Left");
+        }
+    }
+}
